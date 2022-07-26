9037497b7b8568cd6c7f54a73d7a27e7a9ce02b3
Akoruu.Dev <akoruu.dev@gmail.com>
2022-07-26 16:24:29 -0300

    BAS [Bottom]: Add exemple structure about info movie selected on bottom App

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -1,3 +1,24 @@
+let image = "https://www.claquete.com/fotos/filmes/poster/12762_medio.jpg";
+let movieName = "Thor - Amor e Trovão";
+let session = "Quinta-feira - 15:00";
+
+function imageSelected() {
+    return (
+        <div className="bottom-image">
+            <img src={image} alt="image" />
+        </div>
+    )
+}
+
+function infoMovie() {
+    return (
+        <div className="bottom-info">
+            <p>{movieName}</p>
+            <p>{session}</p>
+        </div>
+    )
+}
+
 function Bottom() {
     return(
         <div className="bottom">
