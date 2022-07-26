785c9b7b59aa4519b3496a028c4f9350e1c78dcc
Akoruu.Dev <akoruu.dev@gmail.com>
2022-07-26 14:06:39 -0300

    BAS [Styles]: Add styles components on assets folder and import style files on index.js main

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+import "./assets/styles/style.css";
+import "./assets/styles/colors.css";
+import "./assets/styles/events.css";
+
 ReactDOM.render(<App />, document.querySelector(".root"));
