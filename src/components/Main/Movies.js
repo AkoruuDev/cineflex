654e652752968a9287cf6e314b02159df63dd3ed
Akoruu.Dev <akoruu.dev@gmail.com>
2022-07-27 17:01:36 -0300

    BAS [Get Movies]: Show API folders on Movies.js file from App

--- src/components/Main/Movies.js
+++ src/components/Main/Movies.js
@@ -1,15 +1,31 @@
+import { useEffect, useState } from "react";
 import axios from "axios";
 
+function MovieTemplate({ folder }) {
+    return (
+        <div className="folder">
+            <img src={folder} alt="folder-img" />
+        </div>
+    )
+}
+
 function Movies() {
-    const promise = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
-    promise.then(res => console.log(res.data));
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const promise = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
+        promise.then(res => {
+            setMovies(res.data);
+            console.log(res.data);
+        });
+    }, []);
 
     return (
         <div className="movies">
             <h2 className="page-title">Selecione o filme</h2>
-            {`Lista de Filmes aqui`
-                // <Get page = {page} />
-            }
+            {movies.map(movie => (
+                <MovieTemplate folder={movie.posterURL} />
+            ))}
         </div>
     )
 }
