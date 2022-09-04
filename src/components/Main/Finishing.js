7ed6c129fcd3633bf27bf403a603747b9378e627
Akoruu.Dev <akoruu.dev@gmail.com>
2022-09-04 13:09:09 -0300

    FEAT [Show Seats]: Create a function for show selected seats on finish page

--- src/components/Main/Finishing.js
+++ src/components/Main/Finishing.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 
 function Finishing({ order }) {
+    console.log(order)
     return (
         <div className="finishing">
             <h3 className="finish-title">Pedido feito com sucesso!</h3>
@@ -9,6 +10,9 @@ function Finishing({ order }) {
                 <p>{order.movieName}</p>
                 <p>{order.dayDate} {order.dayHour}</p>
                 <h2 className="finish-subtitle">Ingressos</h2>
+                {order.mySeats.map(seat => (
+                    <p>{`Assento ${seat.name}`}</p>
+                ))}
                 <h2 className="finish-subtitle">Comprador</h2>
                 <p>{`Nome: ${order.nameBuyer}`}</p>
                 <p>{`CPF: ${order.cpfBuyer[0]}${order.cpfBuyer[1]}${order.cpfBuyer[2]}.${order.cpfBuyer[3]}${order.cpfBuyer[4]}${order.cpfBuyer[5]}.${order.cpfBuyer[6]}${order.cpfBuyer[7]}${order.cpfBuyer[8]}-${order.cpfBuyer[9]}${order.cpfBuyer[10]}`}</p>
